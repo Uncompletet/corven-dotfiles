f56080663696f8a21c9d200983055f3765d04ac3
Corven <corvenzett@gmail.com>
2026-09-10 09:35:56 +0200

    Update spicetify-setup.sh

--- spicetify-setup.sh
+++ spicetify-setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR="$HOME/.config"
+
 curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh && \
-    sudo chmod a+wr /opt/spotify 
-    sudo chmod a+wr /opt/spotify/Apps -R
+    sudo chmod a+wr /opt/spotify && \
+    sudo chmod a+wr /opt/spotify/Apps -R && \
+    cp -a "$REPO_DIR/spicetify" "$CONFIG_DIR"
 exit 1
